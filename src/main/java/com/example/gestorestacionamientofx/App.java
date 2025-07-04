57f50fa4d7222e4298e2ef60a512af81722993ca
hermsale <hermsale@gmail.com>
2025-07-03 22:12:01 -0300

    a

--- src/main/java/com/example/gestorestacionamientofx/App.java
+++ src/main/java/com/example/gestorestacionamientofx/App.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Ingreso");
