b62250f59960432f045c502936bc471c35e2f63c
hermsale <hermsale@gmail.com>
2025-07-03 21:12:43 -0300

    se modifico algo

--- src/main/java/com/example/gestorestacionamientofx/Controller/RetirarVehiculoController.java
+++ src/main/java/com/example/gestorestacionamientofx/Controller/RetirarVehiculoController.java
@@ -90,8 +90,8 @@ public class RetirarVehiculoController {
         Duration duracion = Duration.between(fecha_ingreso, fecha_egreso);
 
         // Calculo de tiempo (en horas o días)
-        long minutos = duracion.toMinutes();
-        long horas = (minutos + 59) / 60; // redondea hacia arriba
+//        long minutos = duracion.toMinutes();
+//        long horas = (minutos + 59) / 60; // redondea hacia arriba
 
 //        datos de la cochera - obtengo el nombre del contrato
         String tipoContrato = cochera.getContrato().getNombreContrato();
@@ -100,13 +100,14 @@ public class RetirarVehiculoController {
 //        tomo el tipo de contrato y lo paso a minuscula
         switch (tipoContrato.toLowerCase()) {
             case "por hora" -> {
-                textoDuracion = ("Duración: " + horas + " horas");
+                long horas = Math.max(2, (duracion.toMinutes() + 59) / 60); // redondeo hacia arriba y mínimo 2
+                textoDuracion = "Duración: " + horas + " hora/s";
                 lblDuracion.setText(textoDuracion);
                 precioFinal = precioBaseContrato * horas;
             }
 
             case "por día" -> {
-                long dias = (duracion.toHours() + 23) / 24; // redondeo hacia arriba
+                long dias = Math.max(1, (duracion.toHours() + 23) / 24); // minimo se cobra 1 dia
                 textoDuracion = ("Duración: " + dias + " día/s");
                 lblDuracion.setText(textoDuracion);
                 precioFinal = precioBaseContrato * dias;
